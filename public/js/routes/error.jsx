2e48db70dea63d162f972b413099fa27b0d0f1d5
Martin Ptacek <martin.ptacek@socialbakers.com>
2014-09-07 11:49:22 +0200

    error words

--- public/js/routes/error.jsx
+++ public/js/routes/error.jsx
@@ -3,15 +3,37 @@
 var Error = React.createClass({
 	render: function () {
 		var words = [
-			{key: '404', value: 10},
-			{key: 'not found', value: 9},
-			{key: 'page', value: 7},
-			{key: 'www', value: 6},
-			{key: 'web', value: 5},
+			{key: '404', value: 15},
+			{key: 'not found', value: 12},
+			{key: 'page', value: 10},
+			{key: 'www', value: 8},
+			{key: 'Web', value: 5},
 			{key: 'server', value: 4},
 			{key: 'error', value: 3},
-			{key: 'web caution', value: 2},
-			{key: 'missing', value: 2}
+			{key: 'Web caution', value: 4},
+			{key: 'Missing', value: 5},
+			{key: 'Cloud', value: 4},
+			{key: 'Wordcloud', value: 6},
+			{key: 'required', value: 5},
+			{key: 'NodeJS', value: 6},
+			{key: 'JajaScript', value: 5},
+			{key: 'try', value: 7},
+			{key: 'Incompatible', value: 3},
+			{key: 'Help', value: 4},
+			{key: 'Think', value: 2},
+			{key: 'Offline', value: 5},
+			{key: 'Oops!', value: 7},
+			{key: 'reached', value: 4},
+			{key: 'Alert', value: 7},
+			{key: 'Ponny', value: 1},
+			{key: 'little', value: 1},
+			{key: 'Occurred', value: 6},
+			{key: 'File', value: 4},
+			{key: 'Image', value: 3},
+			{key: 'Unexpected', value: 4},
+			{key: 'Indexing', value: 5},
+			{key: 'Publishing', value: 6},
+			{key: 'was', value: 6}
 		];
 
 		return (
